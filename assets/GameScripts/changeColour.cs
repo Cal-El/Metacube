1e8295faaf19aa7766a1304994c6ae1cf8a09bb9
Callum Grier <cal.grier@gmail.com>
2016-05-21 23:36:28 +1000

    Lightmaps should work with cloud build

--- assets/GameScripts/changeColour.cs
+++ assets/GameScripts/changeColour.cs
@@ -6,6 +6,7 @@ public class changeColour : MonoBehaviour {
 	//public Material[] materialArray = new Material[7];
 	public Material changingMat;
 	public Color colour;
+    private Color currColour;
 	public float speed = 1;
 	public float pulseSpeed = 0.5f;
 	public bool acceptNewColor = true;
@@ -13,12 +14,15 @@ public class changeColour : MonoBehaviour {
 	
 	void Start ()
 	{
-		changingMat.color = colour;
-	}
+		changingMat.SetColor("_EmissionColor", colour);
+        currColour = colour;
+
+    }
 	// Update is called once per frame
 	void Update ()
 	{
-		changingMat.color = Color.Lerp(changingMat.color, colour, speed*0.01f);
+		currColour = Color.Lerp(currColour, colour, speed*0.01f);
+        changingMat.SetColor("_EmissionColor", currColour);
         //transform.renderer.material.mainTexture
         changingMat.mainTextureOffset = new Vector2(0, (changingMat.mainTextureOffset.y+(Time.deltaTime*pulseSpeed))%1);
         
