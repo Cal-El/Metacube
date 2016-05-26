402edfdb440bb0ffcc3691b59460f13f2d5f2ae6
Callum Grier <cal.grier@gmail.com>
2016-05-26 12:23:04 +1000

    Last art unlock

--- assets/ART.cs
+++ assets/ART.cs
@@ -17,12 +17,26 @@ public class ART : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (PlayerPrefs.HasKey(unlockKey))
-            if (PlayerPrefs.GetInt(unlockKey) > 0)
-                r.enabled = true;
+        if (unlockKey.EndsWith("5"))
+        {
+            string chapter = unlockKey.TrimEnd('5');
+            if (PlayerPrefs.HasKey(chapter + "1") && PlayerPrefs.HasKey(chapter + "2") && PlayerPrefs.HasKey(chapter + "3") && PlayerPrefs.HasKey(chapter + "4"))
+                if (PlayerPrefs.GetInt(chapter + "1") > 0 && PlayerPrefs.GetInt(chapter + "2") > 0 && PlayerPrefs.GetInt(chapter + "3") > 0 && PlayerPrefs.GetInt(chapter + "4") > 0)
+                    r.enabled = true;
+                else
+                    r.enabled = false;
             else
                 r.enabled = false;
+        }
         else
-            r.enabled = false;
+        {
+            if (PlayerPrefs.HasKey(unlockKey))
+                if (PlayerPrefs.GetInt(unlockKey) > 0)
+                    r.enabled = true;
+                else
+                    r.enabled = false;
+            else
+                r.enabled = false;
+        }
     }
 }
