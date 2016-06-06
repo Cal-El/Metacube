32c6e545d67f7d7cf7e9ddbd27a0cc07b56f4c69
Callum Grier <cal.grier@gmail.com>
2016-06-06 18:15:03 +1000

    Last trigger and fixed checkpoint

---
+++ assets/CheckpointTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour {
+
+    public int activeInProgress = 0;
+    public int myCheckpoint = 0;
+
+    void OnTriggerEnter(Collider c) {
+        if(GameManager.GM.progression == activeInProgress) {
+            if(c.tag == "Player") {
+                GameManager.GM.checkpointNum = myCheckpoint;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
