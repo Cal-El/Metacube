3d9a54c92709b1e01aeeee3a5567cb136f0d126f
Callum Grier <cal.grier@gmail.com>
2016-08-11 20:29:28 +1000

    Fixed Level music changes. Need to Fix level settings

--- assets/Scripts/LevelMusic.cs
+++ assets/Scripts/LevelMusic.cs
@@ -27,15 +27,17 @@ public class LevelMusic : MonoBehaviour {
             tracks[i].player.loop = true;
             tracks[i].player.volume = 0;
             tracks[i].player.Play();
-            tracks[i].volume = 0;
         }
     }
 
     // Update is called once per frame
     void Update() {
-    	progressionFloat = Mathf.Lerp(progressionFloat, GameManager.GM.progression, Time.deltaTime);
-    	
-    	for (int i = 0; i < tracks.Length; i++) {
+        if (!GameManager.GM.finishedLevel)
+            progressionFloat = Mathf.Lerp(progressionFloat, GameManager.GM.progression, Time.deltaTime);
+    	else
+            progressionFloat = Mathf.Lerp(progressionFloat, GameManager.GM.progression+1, Time.deltaTime);
+
+        for (int i = 0; i < tracks.Length; i++) {
         	tracks[i].player.volume = AudioManager.GetVolume(AudioManager.AUDIO_TYPES.Music) * tracks[i].volumeCurve.Evaluate(progressionFloat);
         }
     }
