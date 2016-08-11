63feb1e1c80ad5a94fca04c9952fb01ba47b5e7e
Cal-El <cal.grier@gmail.com>
2016-08-11 18:40:08 +1000

    Update LevelMusic.cs
    Uses animation curves

--- assets/Scripts/LevelMusic.cs
+++ assets/Scripts/LevelMusic.cs
@@ -5,16 +5,22 @@ public class LevelMusic : MonoBehaviour {
 
     [System.Serializable]
     public struct MusicSection {
+    	[Tooltip("Clip of music to play.")]
         public AudioClip clip;
-        public float volume;
+        [Tooltip("Track volume at level progression x. Cannot exceed 1.")]
+        public AnimationCurve volumeCurve;
+        [HideInInspector]
         public AudioSource player;
     }
 
     public MusicSection[] tracks;
+    private float progressionFloat = 0.0f;
 
 	// Use this for initialization
 	void Start () {
         //AudioSource[] ads = GetComponents<AudioSource>();
+        progressionFloat = GameManager.GM.progression;
+        
         for (int i = 0; i < tracks.Length; i++) {
             tracks[i].player = gameObject.AddComponent<AudioSource>();
             tracks[i].player.clip = tracks[i].clip;
@@ -27,28 +33,10 @@ public class LevelMusic : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (GameManager.GM.checkpointNum < GameManager.GM.checkpoints.Length - 1) {
-            for (int i = 0; i < tracks.Length; i++) {
-                tracks[i].volume = Mathf.Lerp(tracks[i].volume, ConditionToInt(GameManager.GM.progression >= i && !GameManager.GM.finishedLevel), Time.deltaTime);
-                tracks[i].player.volume = AudioManager.GetVolume(AudioManager.AUDIO_TYPES.Music) * tracks[i].volume;
-            }
-        } else if(!GameManager.GM.finishedLevel) {
-            for (int i = 1; i < tracks.Length; i++) {
-                tracks[i].volume = Mathf.Lerp(tracks[i].volume, 0, Time.deltaTime);
-                tracks[i].player.volume = AudioManager.GetVolume(AudioManager.AUDIO_TYPES.Music) * tracks[i].volume;
-            }
-        } else {
-            for (int i = 0; i < tracks.Length; i++) {
-                tracks[i].volume = Mathf.Lerp(tracks[i].volume, 0, Time.deltaTime);
-                tracks[i].player.volume = AudioManager.GetVolume(AudioManager.AUDIO_TYPES.Music) * tracks[i].volume;
-            }
+    	progressionFloat = Mathf.Lerp(progressionFloat, GameManager.GM.progression, Time.deltaTime);
+    	
+    	for (int i = 0; i < tracks.Length; i++) {
+        	tracks[i].player.volume = AudioManager.GetVolume(AudioManager.AUDIO_TYPES.Music) * tracks[i].volumeCurve.Evaluate(progressionFloat);
         }
-    }
-
-    int ConditionToInt(bool b) {
-        if (b)
-            return 1;
-        else
-            return 0;
     }
 }
