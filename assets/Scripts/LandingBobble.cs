45fb3a338e63bf8c3196b19fdf028c37be08b4d4
Callum Grier <cal.grier@gmail.com>
2016-09-01 17:51:54 +1000

    Added jump bobble to the head bobble. Use the TransferPrefab to copy the values

--- assets/Scripts/LandingBobble.cs
+++ assets/Scripts/LandingBobble.cs
@@ -4,40 +4,56 @@ using System.Collections;
 public class LandingBobble : MonoBehaviour {
 	
 	[Tooltip("Animation curve representing the vertical movement of the head bobble on landing. Normalised between 0-1")]
-	public AnimationCurve bobbleAnimation;
-	public bool wasGrounded = true;
+	public AnimationCurve landingAnimation;
+    public AnimationCurve jumpingAnimation;
+    public bool wasGrounded = true;
 	public CharacterMotorC chaCon;
 	
 	private float timer = 0.0f;
 	private float magnitude = 0.0f;
-	private float fullAnimationTime;
-	private bool isPlaying;
+	private float landingAnimationTime;
+    private float jumpingAnimationTime;
+    private bool isPlaying;
+    private bool isJumping;
 	private Camera myCam;
 	private Vector3 localCamPos;
 	
 	void Start(){
 		if(chaCon == null) chaCon = FindObjectOfType<CharacterMotorC>();
-		fullAnimationTime = bobbleAnimation.keys[bobbleAnimation.length-1].time;
-		
-		myCam = GetComponent<Camera>();
+
+        jumpingAnimationTime = jumpingAnimation.keys[jumpingAnimation.length - 1].time;
+        landingAnimationTime = landingAnimation.keys[landingAnimation.length - 1].time;
+
+        myCam = GetComponent<Camera>();
 		localCamPos = myCam.transform.localPosition;
 	}
 	
 	void Update(){
-		if(chaCon.grounded && !wasGrounded){
-			magnitude = -chaCon.movement.velocity.y/chaCon.movement.maxFallSpeed;
-			timer = 0;
-			isPlaying = true;
-		}
+        if (chaCon.canControl && chaCon.grounded && !wasGrounded) {
+            magnitude = -chaCon.movement.velocity.y / chaCon.movement.maxFallSpeed;
+            timer = 0;
+            isPlaying = true;
+            isJumping = false;
+        } else if (chaCon.canControl && wasGrounded && Input.GetButtonDown("Jump")) {
+            magnitude = 1;
+            timer = 0;
+            isPlaying = true;
+            isJumping = true;
+        }
+
+
 		if(isPlaying){
-			if(timer >= fullAnimationTime){
-				myCam.transform.localPosition = localCamPos;
-				isPlaying = false;
-				magnitude = 0.0f;
-			}else{
+			if(isJumping && timer <= jumpingAnimationTime){
+                timer += Time.deltaTime;
+                myCam.transform.localPosition = new Vector3(localCamPos.x, localCamPos.y + jumpingAnimation.Evaluate(timer) * magnitude, localCamPos.z);
+            } else if(!isJumping && timer <= landingAnimationTime) {
 				timer += Time.deltaTime;
-				myCam.transform.localPosition = new Vector3(localCamPos.x, localCamPos.y + bobbleAnimation.Evaluate(timer)*magnitude, localCamPos.z);
-			}
+				myCam.transform.localPosition = new Vector3(localCamPos.x, localCamPos.y + landingAnimation.Evaluate(timer)*magnitude, localCamPos.z);
+			} else {
+                myCam.transform.localPosition = localCamPos;
+                isPlaying = false;
+                magnitude = 0.0f;
+            }
 		}
 		wasGrounded = chaCon.grounded;
 	}
