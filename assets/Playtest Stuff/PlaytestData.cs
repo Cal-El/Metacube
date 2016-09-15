be3c7e4884ea2c1ba512bd3abd5fa2a80ec901f2
Cal-El <cal.grier@gmail.com>
2016-09-15 18:48:36 +1000

    Updated Playtest Data
    Should not add to, not overwrite the previous playtest Data

--- assets/Playtest Stuff/PlaytestData.cs
+++ assets/Playtest Stuff/PlaytestData.cs
@@ -9,6 +9,7 @@ public class PlaytestData : MonoBehaviour {
 
     public static PlaytestData PTData;
     protected static float startTime;
+    protected static float timeLastSession = 0;
     protected string userID;
     protected int totalDeathsThisCheckpoint;
     protected int totalDeaths;
@@ -29,7 +30,7 @@ public class PlaytestData : MonoBehaviour {
         public EventData(string _name) {
             name = _name;
             timeStamp = System.DateTime.Now.ToString("HH:mm:ss");
-            timeSinceOpen = Time.time - startTime;
+            timeSinceOpen = Time.time - startTime + timeLastSession;
         }
 
         public override string ToString() {
@@ -49,6 +50,15 @@ public class PlaytestData : MonoBehaviour {
             totalDeaths = PTData.totalDeaths;
             totalDeathsThisCheckpoint = PTData.totalDeathsThisCheckpoint;
         }
+        
+        public DeathEvent (string _name, string _levelName, int _checkpointNum, int _totalDeathsInCheckpoint, int _totalDeathsInLevel, string _timeStamp, float _time) : base(_name) {
+            levelName = _levelName;
+            currentCheckpointNum = _checkpointNum;
+            totalDeaths = _totalDeathsInLevel;
+            totalDeathsThisCheckpoint = _totalDeathsInCheckpoint;
+            timeStamp = _timeStamp;
+        	timeSinceLoad = _time;
+        }
 
         public override string ToString() {
             string s = base.ToString();
@@ -60,6 +70,11 @@ public class PlaytestData : MonoBehaviour {
     public class ApplicationEvent : EventData {
         public ApplicationEvent(string _name) : base(_name) {
 
+        }
+        
+        public ApplicationEvent(string _name, string _timeStamp, float _time) : base(_name){
+        	timeStamp = _timeStamp;
+        	timeSinceLoad = _time;
         }
 
         public override string ToString() {
@@ -75,6 +90,13 @@ public class PlaytestData : MonoBehaviour {
             levelName = _levelName;
             checkpointNum = _checkpointNum;
         }
+        
+        public CheckpointEvent(string _name, string _levelName, int _checkpointNum, string _timeStamp, float _time) : base(_name){
+        	levelName = _levelName;
+            	checkpointNum = _checkpointNum;
+        	timeStamp = _timeStamp;
+        	timeSinceLoad = _time;
+        }
 
         public override string ToString() {
             return base.ToString() + "\t" + levelName + "\t" + checkpointNum;
@@ -85,6 +107,11 @@ public class PlaytestData : MonoBehaviour {
 
         public CheatingEvent(string _name) : base(_name) {
             
+        }
+        
+        public CheatingEvent(string _name, string _timeStamp, float _time) : base(_name){
+        	timeStamp = _timeStamp;
+        	timeSinceLoad = _time;
         }
 
         public override string ToString() {
@@ -127,6 +154,7 @@ public class PlaytestData : MonoBehaviour {
         if (PTData != null) {
             startTime = Time.time;
             PTData.userID = ID;
+            LoadData();
             PTData.currentCheckpoint = -1;
             PTData.totalDeaths = 0;
             PTData.totalDeathsThisCheckpoint = 0;
@@ -225,6 +253,67 @@ public class PlaytestData : MonoBehaviour {
             }
 
             sw.Close();
+        }
+    }
+    
+    public static void LoadData() {
+        if (PTData != null) {
+            string directoryPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\Metacube\\TestData";
+            if (!Directory.Exists(directoryPath)) {
+                Directory.CreateDirectory(directoryPath);
+            }
+            if(!File.Exists(directoryPath + "\\User" + PTData.userID + ".txt")){
+            	return;	
+            }
+            
+            StreamReader sr = new StreamReader(directoryPath + "\\User" + PTData.userID + ".txt");
+            string line = sr.ReadLine();
+            float totalTimeLastSession = 0;
+            
+            if(sr.ReadLine() == "Application Events"){
+            	sr.ReadLine(); //Timestamp Time Event
+            	line = sr.ReadLine();
+            	while(line.Length > 0){
+            		string[] parts = line.Split("\t")
+            		PTData.events.Add(new ApplicationEvent(parts[2], parts[0], float.Parse(parts[1])));
+            		totalTimeLastSession = float.Parse(parts[1]);
+            		line = sr.ReadLine();
+            	}
+            }
+            
+            if(sr.ReadLine() == "Checkpoint Events"){
+            	sr.ReadLine(); //Timestamp Time Progressed Level Name Checkpoint
+            	line = sr.ReadLine();
+            	while(line.Length > 0){
+            		string[] parts = line.Split("\t");
+            		PTData.events.Add(new CheckpointEvent(parts[2], parts[3], int.Parse(parts[4]), parts[0], float.Parse(parts[1])));
+            		line = ReadLine();
+            	}
+            }
+            
+            if(sr.ReadLine() == "Death Events"){
+            	sr.ReadLine(); //Timestamp	Time	Event	LevelName	Checkpoint	Deaths this Checkpoint
+            	line = sr.ReadLine();
+            	while(line.Length > 0){
+            		string[] parts = line.Split("\t");
+            		PTData.events.Add(new DeathEvent(parts[2], parts[3], int.Parse(parts[4]), int.Parse(parts[5]), int.Parse(parts[6]), parts[0], float.Parse(parts[1])));
+            		line = ReadLine();
+            	}
+            }
+            
+            if(sr.ReadLine() == "Cheat Events"){
+            	sr.ReadLine(); //Timestamp	Time	Cheat Used
+            	line = sr.ReadLine();
+            	while(!sr.EndOfStream && line.Length > 0){
+            		string[] parts = line.Split("\t");
+            		PTData.events.Add(new CheatingEvent(parts[2], parts[0], float.Parse(parts[1])));
+            		line = ReadLine();
+            	}
+            }
+
+	
+	    timeLastSession = totalTimeLastSession;
+            sr.Close();
         }
     }
 }
